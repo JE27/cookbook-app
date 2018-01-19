7900b3d6b0755a0b5f3ca1990762d5feb3fbbc56
je27 <jocmoya@gmail.com>
2018-01-18 21:29:16 -0600

    added all of the things

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,43 @@
 class RecipesController < ApplicationController
-  def one_recipe_method
-    recipe = Recipe.first
-    render json:{ 
-                 title: recipe.title,
-                 chef: recipe.chef,
-                 ingredients: recipe.ingredients,
-                 directions: recipe.directions 
-                }
+  
+  def index
+    recipes = Recipe.all 
+    render json: recipes.as_json
   end
+
+  def create
+    recipes = Recipe.new(
+                        title: params[:title],
+                        chef: params[:chef],
+                        ingredients: params[:ingredients],
+                        directions: params[:directions]
+                        )
+    recipes.save
+    render json: recipe.as_json
+  end
+
+  def show
+    recipes = Recipe.find(params[:id])
+    render json: recipes.as_json
+  end
+
+  def update
+    recipe = Recipe.find(params[:id]) #find object
+
+    recipe.title = params[:title]   #overrding and changing all attributes 
+    recipe.chef = params[:chef]
+    recipe.ingredients = params[:ingredients]
+    recipe.directions = params[:directions]
+    recipe.save #save udpates
+
+    render json: recipe.as_json #make everything show up as json
+  end
+
+def destroy
+  recipes = Recipe.find(params[:id])
+  recipe.destroy
+  render json: {message: "Successfully destroyed Recipe ##{recipe.id}."}
+end
+
+
 end
